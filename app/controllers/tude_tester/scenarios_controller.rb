a160856e74452b5f1c2901fc5e05cd266dcd57db
Richard LaFranchi <rlafranchi@icloud.com>
2017-06-15 08:32:10 -0600

    scenarios endpoints

--- app/controllers/tude_tester/scenarios_controller.rb
+++ app/controllers/tude_tester/scenarios_controller.rb
@@ -8,5 +8,41 @@ module TudeTester
         methods: [:feature, :to_s]
       )
     end
+
+    def create
+      scenario = Scenario.new(scenario_params)
+      if scenario.save
+        render json: scenario.to_json(
+            include: { steps: { methods: [:friendly_type, :parent_type] } },
+            methods: [:feature, :to_s]
+        )
+      else
+        render json: { errors: scenario.errors }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      scenario = Scenario.find(params[:id])
+      if scenario.update(scenario_params)
+        render json: scenario.to_json(
+            include: { steps: { methods: [:friendly_type, :parent_type] } },
+            methods: [:feature, :to_s]
+        )
+      else
+        render json: { errors: scenario.errors }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      Scenario.find(params[:id]).destroy
+      render head: :ok
+    end
+
+    private
+
+    def scenario_params
+      params.require(:scenario).permit(:title, :tude_tester_feature_id)
+    end
+
   end
 end
